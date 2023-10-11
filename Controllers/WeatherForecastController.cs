2769b2d649fe3297a7443924b4515e94553676ca
Fabio Padua <fabiopadua@microsoft.com>
2023-10-11 09:00:52 -0300

    chore: added CosmosDB connection

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using DotNetEnv;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 
 namespace SampleCosmosDB.Controllers;
 
@@ -16,17 +18,59 @@ public class WeatherForecastController : ControllerBase
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+        Env.Load();
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<IEnumerable<WeatherForecast>> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return await getWheater();
+        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        // {
+        //     Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //     TemperatureC = Random.Shared.Next(-20, 55),
+        //     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        // })
+        // .ToArray();
+    }
+
+    private async Task<IEnumerable<WeatherForecast>> getWheater() {
+
+        var connectionString = Environment.GetEnvironmentVariable("COSMOSDB_CONNECTION_STRING");
+
+        using CosmosClient client = new(
+            connectionString: connectionString!
+        );
+
+        var container = client.GetContainer("cosmos","first");
+
+        // Query multiple items from container
+        //using FeedIterator<WeatherForecast> feed = container.GetItemQueryIterator<WeatherForecast>(
+        //    queryText: "SELECT * FROM first"
+        //);
+
+        // Execute query and return results as list
+        var results = new List<WeatherForecast>();
+        var iterator = container.GetItemQueryIterator<WeatherForecast>("SELECT * FROM first");
+
+        while (iterator.HasMoreResults)
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            var response = await iterator.ReadNextAsync();
+            results.AddRange(response);
+        }
+
+        return results;
+
+        // Iterate query result pages
+        // while (feed.HasMoreResults)
+        // {
+        //     FeedResponse<WeatherForecast> response = await feed.ReadNextAsync();
+
+        //     // Iterate query results
+        //     foreach (WeatherForecast item in response)
+        //     {
+        //         Console.WriteLine($"Found item:\t{item.name}");
+        //     }
+        // }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+Env.Load("dev.env");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
